cfd975961389e89a47755b4c29cbffae790d3a6d
Yash Sukhwani <43533666+YashSukhwani@users.noreply.github.com>
2020-08-30 15:19:03 -0500

    Update Eliza.java

--- Eliza.java
+++ Eliza.java
@@ -1,26 +1,3 @@
-//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
-//
-// Title:           (descriptive title of the program making use of this file)
-// Files:           (a list of all source files used by that program)
-// Course:          CS 200, Fall 2018
-//
-// Author:          Yash Sukhwani
-// Email:           sukhwani@wsic.edu
-// Lecturer's Name: Marc Renault
-//
-///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
-//
-// Students who get help from sources other than their partner must fully 
-// acknowledge and credit those sources of help here.  Instructors and TAs do 
-// not need to be credited here, but tutors, friends, relatives, room mates 
-// strangers, etc do.  If you received no outside help from either type of 
-// source, then please explicitly indicate NONE.
-//
-// Persons:         (identify each person and describe their help in detail)
-// Online Sources:  (identify each URL and describe their assistance in detail)
-//
-/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
-
 /**
  * @author yashsukhwani
  *
